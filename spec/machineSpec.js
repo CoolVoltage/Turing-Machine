6360c026cd20c89975eb5cd73dc17bfb902922ed
Karthik Vijay <vijaypcguy@gmail.com>
2015-07-04 01:37:58 +0530

    read operation on tape

--- spec/machineSpec.js
+++ spec/machineSpec.js
@@ -1,25 +1,50 @@
-require('../src/machine.js')
+var rewire = require('rewire')
+var turingMachine = rewire('../src/machine.js')
 
 describe('Error Handlers in machine.js',function(){
 	it("addIntruction",function(){
-		expect(function(){ (new turingMachine).addInstruction({}); }).toThrow(new Error("Instruction not structured properly."));	
+		expect(function(){ turingMachine.addInstruction({}); }).toThrow(new Error("Instruction not structured properly."));	
 	});
 
 });
 
-describe('Actual work',function(){
+describe('Tape operations',function(){
+	var tape = turingMachine.__get__("tape");
+
+	it("blankTape",function(){
+		expect(function(){
+			return tape.readTape(-1);
+		}())
+		.toEqual("B")
+	});
+
+	it("symbolExists",function(){
+		expect(function(){
+			tape.leftStack = ["A"];
+			tape.rightStack = ["C"];
+			
+			return tape.readTape(-1);		
+		}())
+		.toEqual("A");	
+	});
+	
+});
+
+describe('Public Functions',function(){
 	
 	it("addInstruction",function(){
 		expect(function(){
-			var mac = new turingMachine();
+			var mac =turingMachine;
 			var set = {
 				currentState : "0",
 				valueOnStack : "1",
 				replacementValue : "A",
 				headMovement : "L"		
+			
 			};
 			return mac.addInstruction(set);
 		}())
 		.toEqual(jasmine.any(Number));
 	});	
+
 });
--- src/machine.js
+++ src/machine.js
@@ -1,31 +1,50 @@
-(function(){
-	// The turing machine
-	turingMachine = function(){
-		
-		//The head of the machine varies -MAX_SAFE_INTEGER to +MAX_SAFE_INTEGER
-		var head = 0;
+// The turing machine
 
-		//Tape is represented by two stacks
-		// leftStack's growth represent tape growth on the left
-		// rightStack's growth represent tape growth on the right
-		var leftStack=[],rightStack=[];
+	//The head of the machine varies -MAX_SAFE_INTEGER to +MAX_SAFE_INTEGER
+	var head = 0;
+	//Tape is represented by two stacks
+	// leftStack's growth represent tape growth on the left
+	// rightStack's growth represent tape growth on the right
+	var tape = new (function(){
+			this.leftStack=[],this.rightStack=[];
 
-		//Current state of the machine
-		var currentState;
+			this.readTape = function(position){
 
-		//Table of instructions
-		//Each instruction is denoted as {currentState,valueOnStack,replacementValue,headMovement}
-		var table=[];
-		
-		//To add instruction sets to the table
-		//No replacementValue signifies no replacement
-		//No headMovement signifies HALT
-		this.addInstruction = function(instruction){
-			if(!(instruction && instruction.currentState && instruction.valueOnStack ))
-				throw new Error("Instruction not structured properly.");
-			return table.push(instruction);		
-		}
-	};
+			var absPosition,stack;
 
-	
-})();
+			if(position < 0){
+				// -1 is 0 on leftStack
+				absPosition = position*-1 - 1;
+				stack = this.leftStack;
+
+			}else{
+				// 0 is 0 on rightStack
+				absPosition = position;
+				stack = this.rightStack;
+			}	
+
+			//B is reserved for blank
+			if(stack.length <= absPosition)
+				return "B";
+			else
+				return stack[absPosition];
+
+			}
+	})();
+	//Current state of the machine
+	var currentState;
+
+	//Table of instructions
+	//Each instruction is denoted as {currentState,valueOnStack,replacementValue,headMovement}
+	var table=[];
+
+	//To add instruction sets to the table
+	//No replacementValue signifies no replacement
+	//No headMovement signifies HALT
+	addInstruction = function(instruction){
+		if(!(instruction && instruction.currentState && instruction.valueOnStack ))
+			throw new Error("Instruction not structured properly.");
+		return table.push(instruction);		
+	}
+
+module.exports.addInstruction = addInstruction;
